1a4b6c1d8387ae81b4dfb9e2ca3d3a8743ae64e3
Colleen <153748501+colleennicole223@users.noreply.github.com>
2024-04-02 05:25:26 -0900

    Added boilerplate to index and formatted stylesheet

---
+++ script.js
@@ -0,0 +1,14 @@
+
+//Main
+getComputerChoice()
+
+
+//Functions
+function getComputerChoice() {
+
+    let randomChoice = Math.random()
+    console.log(randomChoice)
+    console.log("Rock")
+    console.log("Paper")
+    console.log("Scissors")
+}
